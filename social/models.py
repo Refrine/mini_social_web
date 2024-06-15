d0dcafa2cb2e37e81aeeb5bfea45eadb8a8c8a2c
Refrine <refrine123@bk.ru>
2024-06-15 15:44:55 +0300

    follow

--- social/models.py
+++ social/models.py
@@ -30,8 +30,16 @@ class Post(models.Model):
         return self.user
     
 class LikePost(models.Model):
-    post_id = models.CharField( max_length=500)
+    post_id = models.CharField(max_length=500)
     user = models.CharField(max_length=100)
     
     def __str__(self) -> str:
         return self.username
+    
+    
+class FollowersCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return self.user
--- social/urls.py
+++ social/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('upload/', views.upload, name='upload'),
     path('like/', views.like, name='like'),
     path('profile/<str:pk>/', views.profile, name='profile'),
+    path('follow/', views.follow, name='follow'),
 ]
